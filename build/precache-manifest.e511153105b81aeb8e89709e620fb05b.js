b9b1f7e532bf700ae8ded2a209d7be068ce2013d
masterdrue <masterdrue@gmail.com>
2020-11-15 20:33:32 -0500

    Build 1.03

---
+++ build/precache-manifest.e511153105b81aeb8e89709e620fb05b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239294f2a34936a092ced1440c1b96fc",
+    "url": "/covidWatchII/index.html"
+  },
+  {
+    "revision": "500d24900a20c6bcffd0",
+    "url": "/covidWatchII/static/css/main.94839b4e.chunk.css"
+  },
+  {
+    "revision": "24888d6d4b48444f52c3",
+    "url": "/covidWatchII/static/js/2.a0577574.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidWatchII/static/js/2.a0577574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500d24900a20c6bcffd0",
+    "url": "/covidWatchII/static/js/main.1b8f4c7f.chunk.js"
+  },
+  {
+    "revision": "a80b4a7e6b7efb0ef2ab",
+    "url": "/covidWatchII/static/js/runtime-main.c574cf77.js"
+  },
+  {
+    "revision": "ad9e5ff197c76f91e016bbab3a030074",
+    "url": "/covidWatchII/static/media/cv19.ad9e5ff1.png"
+  },
+  {
+    "revision": "79f77b5967f706d336e6f2a7150e0a3d",
+    "url": "/covidWatchII/static/media/logo-250.79f77b59.png"
+  }
+]);
